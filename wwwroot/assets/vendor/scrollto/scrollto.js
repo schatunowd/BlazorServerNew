d81fbb50c307817f8c06b2264b764fdc19c2b1e4
xochyymeret <schatunowdima@yandex.ru>
2022-05-28 16:26:55 +0300

    add scroll

---
+++ wwwroot/assets/vendor/scrollto/scrollto.js
@@ -0,0 +1,11 @@
+
+    function BlazorScrollToId(id) {
+        const element = document.getElementById(id);
+        if (element instanceof HTMLElement) {
+            element.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+                inline: "nearest"
+            });
+        }
+    }
